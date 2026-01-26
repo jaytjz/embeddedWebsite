ef113ec9c9326c7043ef6c5cbd5265e4a452d913
jaytjz <jeremytanjanzhe@gmail.com>
2026-01-26 11:18:07 +0000

    init

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="min-h-screen grid place-items-center">
+      <h1 className="text-4xl font-bold underline">Tailwind works ✅</h1>
+    </div>
+  );
+}
